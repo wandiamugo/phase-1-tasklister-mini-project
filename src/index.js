b7c912f66ca2d7ea6fb46c151105f5cd6f815547
Wandia E Mugo <wwandia173@gmail.com>
2022-06-13 23:42:43 +0300

    add index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('Create New Task', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new-task-description.value)
+    form.reset()
+  })
 });
+
+function buildToDo(activity){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent= 'x'
+  p.textContent = `${activity}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
